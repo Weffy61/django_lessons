3607e1ac9ec59e0d511f1fd7bc5f76c7b88c1975
Weffy61 <weffy@mail.ru>
2021-12-10 00:51:24 +0300

    changes

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -104,12 +104,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
+# 123
 
 LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
+
 
 USE_TZ = False
 
